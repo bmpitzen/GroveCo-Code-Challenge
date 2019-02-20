7c6e67fd6169288f22bd6e13b6719ff73fc2ea15
Brandon Pitzen <bmpitzen@icloud.com>
2019-02-20 11:21:16 -0500

    Fixed issue in 'Options' for address.

--- find_store.rb
+++ find_store.rb
@@ -11,11 +11,11 @@ Usage:
   #{__FILE__} --zip='<zip>' [--units=(mi|km)] [--output=text|json]
 
 Options:
-  --zip=<zip>          Find nearest store to this zip code. If there are multiple best-matches, return the first.
-  --address            Find nearest store to this address. If there are multiple best-matches, return the first.
-  --units=(mi|km)      Display units in miles or kilometers [default: mi]
-  --output=(text|json) Output in human-readable text, or in JSON (e.g. machine-readable) [default: text]
-   -h, --help                   Show this message.
+  --zip=<zip>               Find nearest store to this zip code. If there are multiple best-matches, return the first.
+  --address=<address>       Find nearest store to this address. If there are multiple best-matches, return the first.
+  --units=(mi|km)           Display units in miles or kilometers [default: mi]
+  --output=(text|json)      Output in human-readable text, or in JSON (e.g. machine-readable) [default: text]
+   -h, --help               Show this message.
 
 DOCOPT
 
